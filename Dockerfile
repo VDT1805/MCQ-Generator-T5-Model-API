f51be5b3c9914a2cd455a24f7f6f4973051665f6
VDT1805 <136230459+VDT1805@users.noreply.github.com>
2024-06-02 10:42:52 +0700

    with distractor

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,49 @@
 # COPY . /app
 # RUN pip --no-cache-dir install -r requirements.txt
 # CMD ["python3", "app.py"]
-FROM python:3.10
+# FROM python:3.10
+# # Create a new group and user with a home directory
+# RUN groupadd -r demouser && useradd -r -m -g demouser demouser
+
+# # Create a new directory and set the necessary permissions
+# RUN mkdir /app && chown -R demouser:demouser /app
+
+# # Switch to the new user
+# USER demouser
+
+# # Set the working directory
+# WORKDIR /app
+
+# # Copy the current directory contents into the container at /app
+# COPY --chown=demouser:demouser . /app
+
+# # Install any needed packages specified in requirements.txt
+# RUN pip install --no-cache-dir -r requirements.txt
+
+# # Run app.py when the container launches
+# CMD ["python3", "app.py"]
+# Use python slim
+FROM python:3.10-slim
+
+# Set the working directory within the container
 WORKDIR /app
+
+# Copy necessary files to the container
 COPY . /app
-RUN pip install --no-cache-dir -r requirements.txt
-CMD ["python3", "app.py"]
+
+# Create a virtual environment in the container
+RUN python3 -m venv .venv
+
+# Activate the virtual environment
+ENV PATH="/app/.venv/bin:$PATH"
+
+    # Install Python dependencies from the requirements file
+RUN pip install --no-cache-dir -r requirements.txt 
+
+# Make port 5000 available to the world outside this container
+EXPOSE 5000
+
+ENTRYPOINT [ "python3" ]
+
+# Run app.py when the container launches
+CMD [ "app.py" ]
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from typing import OrderedDict
 import flask
 from flask import request, jsonify
 from transformers import (
@@ -6,6 +7,14 @@ from transformers import (
     T5Tokenizer
 )
 import json
+import spacy
+from sense2vec import Sense2Vec
+nlp = spacy.load("en_core_web_md")
+s2v = nlp.add_pipe("sense2vec")
+s2v.from_disk("s2v_old") 
+# s2v = Sense2Vec().from_disk("s2v_old")
+
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 trained_model_path = 'models/qag_t5_trained_model_v7'
@@ -14,6 +23,36 @@ trained_tokenizer = 'models/qag_t5_tokenizer_v7'
 model = T5ForConditionalGeneration.from_pretrained(trained_model_path)
 tokenizer = T5Tokenizer.from_pretrained(trained_tokenizer)
 
+def sense2vec_get_words(word,s2v):
+    output = []
+    word = word.lower()
+    word = word.replace(" ", "_")
+
+    sense = s2v.get_best_sense(word)
+    most_similar = s2v.most_similar(sense, n=5)
+
+    # print ("most_similar ",most_similar)
+
+    for each_word in most_similar:
+        append_word = each_word[0].split("|")[0].replace("_", " ").lower()
+        if append_word.lower() != word:
+            output.append(append_word.title())
+
+    out = list(OrderedDict.fromkeys(output))
+    return out
+
+def extract_token_by_indices(doc, start, end):
+    
+    # Extract the span using the start and end indices
+    span = doc.char_span(start, end)
+    
+    if span is not None:
+        # # Extract the token within the span
+        # tokens = [token for token in span]
+        # return tokens
+        return span
+    else:
+        return None
 
 def from_string(formatted_string):
   parts = formatted_string.split("answer:")
@@ -26,6 +65,8 @@ def from_string(formatted_string):
 
 def get_question(mdl,tknizer,sentence,answers=["[MASK]"],num_of_q=1):
     questions = []
+    # Process the sentence with spaCy
+    doc = nlp(sentence)
     for answer in answers:
         text = "context: {} answer: {}".format(sentence,answer)
         # print (text)
@@ -44,12 +85,57 @@ def get_question(mdl,tknizer,sentence,answers=["[MASK]"],num_of_q=1):
 
         decs = [tknizer.decode(ids,skip_special_tokens=True) for ids in outs]
         ques = []
-        for dec in decs:
-            # ques.append('question: {} answer: {}'.format(dec,answer))
+        for index,dec in enumerate(decs):
             Question, answer = from_string(dec)
-            ques.append({'question': Question , 'ans1': answer})
+            print("Answer: ", answer)
+            if answers[0][0] == "[MASK]":
+                start = sentence.find(answer)
+                end = start + len(answer)
+            else:
+                # print(answers[index],"DSK")
+                start = int(answers[index][1])
+                end = int(answers[index][2])
+            # print("Start: ", start, "End: ", end)
+            tokens = extract_token_by_indices(doc, start, end)
+            distractions = ["","","","",""]
+            try:
+                distractions = tokens._.s2v_most_similar(5)
+                distractions = [distract[0][0] for distract in distractions]
+            except ValueError as e:
+                distractions = ["","","","",""]
+            distractions += [''] * (5 - len(distractions))
+            ques.append({'question': Question , 'ans1': answer, 'ans2': distractions[0], 'ans3': distractions[1], 'ans4': distractions[2], 'ans5': distractions[3], "ans6": distractions[4]})
         questions = questions + ques
     return questions
+
+# def get_question(mdl,tknizer,sentence,answers=["[MASK]"],num_of_q=1):
+#     questions = []
+#     for answer in answers:
+#         text = "context: {} answer: {}".format(sentence,answer)
+#         # print (text)
+#         max_len = 512
+#         encoding = tknizer.encode_plus(text,max_length=max_len, pad_to_max_length=False,truncation=True, return_tensors="pt")
+
+#         input_ids, attention_mask = encoding["input_ids"], encoding["attention_mask"]
+
+#         outs = mdl.generate(input_ids=input_ids,
+#                                     attention_mask=attention_mask,
+#                                     early_stopping=True,
+#                                     num_beams=5,
+#                                     num_return_sequences=num_of_q,
+#                                     no_repeat_ngram_size=2,
+#                                     max_length=72)
+
+#         decs = [tknizer.decode(ids,skip_special_tokens=True) for ids in outs]
+#         ques = []
+#         for dec in decs:
+#             # ques.append('question: {} answer: {}'.format(dec,answer))
+#             Question, answer = from_string(dec)
+#             distractors = sense2vec_get_words(answer,s2v)
+
+#             ques.append({'question': Question , 'ans1': answer, 'ans2': distractors[0]})
+#         questions = questions + ques
+#     return questions
 
 # context = """
 # Pinocchio ( pin-OH-kee-oh, Italian: ) is a fictional character and the protagonist of the children's novel The Adventures of Pinocchio (1883) by Italian writer Carlo Collodi of Florence, Tuscany. Pinocchio was carved by a woodcarver named Geppetto in a Tuscan village. He is created as a wooden puppet, but he dreams of becoming a real boy. He is known for his long nose, which grows when he lies.
@@ -58,13 +144,17 @@ def get_question(mdl,tknizer,sentence,answers=["[MASK]"],num_of_q=1):
 # qs = get_question(model, tokenizer, context)
 # print(qs)
 
+
+# In electronics and computer science, a reduced instruction set computer (RISC) is a computer architecture designed to simplify the individual instructions given to the computer to accomplish tasks. Compared to the instructions given to a complex instruction set computer (CISC), a RISC computer might require more instructions (more code) in order to accomplish a task because the individual instructions are written in simpler code. The goal is to offset the need to process more instructions by increasing the speed of each instruction, in particular by implementing an instruction pipeline, which may be simpler to achieve given simpler instructions.
 @app.route('/genqa', methods=['POST'])
 def generate_question():
     data = request.get_json()
     sentence = data.get('context')
     answers = data.get('answers')
-    num_of_q = int(data.get('num_of_q')) 
+    num_of_q = int(data.get('num_of_q'))
+    print(answers)
     questions = get_question(model, tokenizer,sentence, answers ,num_of_q)
+    print(questions)
     return json.dumps(questions)
 
 if __name__ == '__main__':
